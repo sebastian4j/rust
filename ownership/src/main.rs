bd6e6d2fcc2f60a368e829acf122dec7bf2aa474
Sebastián Ávila A <sebastian4j@gmail.com>
2020-10-28 15:09:26 -0300

    faltantes

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -37,6 +37,8 @@ para guardar la nueva data, la ubicación está siempre
 
  */
 
+use std::ops::Add;
+
 fn main() {
     // scope: rango dentro del programa en el cual un item es valido
     {   // s no es valido
@@ -50,9 +52,72 @@ fn main() {
     // - string literal es inmutable y se guarda en el stack
     // - String se guarda en el heap y permite guardar el string que es desconocido
     //   en tiempo de compilación y es mutable
-    // - se puede crear un String con nun string literal:
+
+    // - se puede crear un String con un string literal:
     let mut s = String::from("hola");
     s.push_str(" mundo!");
     println!("{}", s);
 
+
+    let x = 5;
+    let y = x;
+    // y es una copia de y: como es un valor fijo y conocido en tiempo de compilación y vive en el stack se copia
+    // cuando el tipo usa el trait Copy se mantienen las variables aún después de copiarlas (el antiguo al nuevo), pero
+    // un tipo no puede usar el trait Copy si el o alguna de sus partes implementa el trait Drop, será un error de compilación
+/*
+     valores escalares simpes son Copy
+     nada que requiera allocation es Copy
+     con Copy:
+     - todos os tipos enteros
+     - los booleanos: bool
+     - los caracteres: char
+     - todos los tipos flotantes
+     - tupas si solo contienen tipos que son Copy
+*/
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    /*
+     un string esta hecho de 3 partes:
+     - un puntero a la memoria que mantiene el contenido del string
+     - largo actual del string en bytes
+     - capacidad total de memoria en bytes que el string ha recibido del sistema operativo
+
+     ese grupo de datos se guarda en el stack, el contenido del string se guarda en el heap
+
+     - cuando se asigna s2 = s1 no se copia lo que está en el heap (el contenido del string)
+       solo lo que está en el stack (puntero, largo y capacidad) == shallow copy
+       PERO se invalida s1, no se puede seguir usando -  rust no creará una deep copy
+*/
+    // cuando una variable está fuera de alcance rust llama a la función 'drop' y a borra la variable del heap
+
+    println!("string: {} x: {} y: {}", s2, x, y); // NO se puede usar s1, fue invalidada
+    let s3 = s2.clone();
+    println!("clonados: {} {} ", s2, s3); // al clonarlos se copia el heap y no se invalida s2
+
+    let s4 = entregar_ownership();
+    println!("s4: {}", s4);
+    let s5 = obtiene_y_regresa(s4); // acá se pierde el s4, fue movido a 'obtiene_y_regresa'
+    println!("nuevo: {}", s5); // s4 ya no existe
+
+    let (s6, largo) = calcular_largo(s5); // asigno el string a s6 para no perder s5
+    println!("el string recuperado: {} el largo: {}", s6, largo);
+}
+
+/** calcula el largo de un string. */
+fn calcular_largo(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+    (s, length) // retorno el string para no perderlo
+}
+
+/** creará un string y lo retornara entregando el ownership a quien lo llame */
+fn entregar_ownership() -> String {
+    let str = String::from("hola");
+    println!("nuevo string creado: {}", str);
+    str
+}
+
+fn obtiene_y_regresa(str: String) -> String {
+    println!("string obtenido: {}", str);
+    str
 }
---
+++ references/src/main.rs
@@ -0,0 +1,58 @@
+fn main() {
+    let str = String::from("hola");
+    let largo = calcular_largo(&str);
+    println!("str: {} lrg: {}", str, largo);
+    // con & se pasa hace referencia a un valor sin tomar su ownership
+    // sin usar el & es dereferencing
+    let mut mutable = String::from("str");
+    println!("mutable: {}", mutable);
+    referencia_mutable(&mut mutable);
+    println!("mutable: {}", mutable);
+
+    // no se puede tener mas de una referencia mutable en un
+    // scope en particuar
+    let m1 = &mut mutable;
+    // let m2 = &mut mutable; // error de compilación xq m1 se ocupa abajo, si no se ocupara se podría definir
+    m1.push_str(" mut ");
+    println!("m1: {}", m1);
+
+    {
+        let m3 = &mut mutable;
+        println!("m3: {}", m3);
+    }
+
+    let m2 = &mut mutable;
+    let m4 = &mut mutable;
+    let m5 = &mut mutable;
+    // println!("{} {} {}", m2, m4, m5); // error de compilación xq se está usando m4 y m5 y son referencia mutables 
+    /*
+     todos podrian cambiar el string y rust evita el 'data race':
+     - 2 o + punteros estan modificando la misma data
+     - al menos un puntero se está usando para escribir data
+     - no hay mecanismo usado para sincronizar la data
+     */
+    // como son referencia no mutables no tiene restricción
+    let nomutable1 = &mutable;
+    let nomutable2 = &mutable;
+    println!("no mutable {} - {} ", nomutable1, nomutable2);
+
+    // NO se puede tener una referencia mutable cuando existe una inmutable (no importa si se crea m7 primero y m6 después)
+    // - explicación: si estoy ocupando una referencia inmutable y de pronto la referencia inmutable lo cambia no sería bueno
+    let m6 = &mutable;
+    // let m7 = &mut mutable;
+    // println!("inmutable: {} mutable: {}", m6, m7); // acá se ocupa m6 y m7, se produce el error x cómo de crearon anteriormente
+    let m8 = &mutable;
+    println!("{} {} ", m6, m8); // m6 y m8 son inmutables, no hay problemas
+
+    // rust asegura que no quedarán referencias perdidas/colgadas (dangling references)
+}
+
+// borrowing: tener las referencias como parametros de funciones
+fn calcular_largo(s: &String) -> usize {
+    s.len()
+} // a referencia queda fuera del scope, pero nada pasa xq no
+// tiene la propiedad de lo que refiere
+
+fn referencia_mutable(s: &mut String) {
+    s.push_str(" mutable ");
+}
